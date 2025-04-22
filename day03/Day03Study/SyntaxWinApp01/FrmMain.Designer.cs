a268211bd7d7446e10611492195b7a92b91592e4
BoKyung Ryu <dbqhrud5789@naver.com>
2025-04-22 17:09:50 +0900

    3일차 커밋

---
+++ day03/Day03Study/SyntaxWinApp01/FrmMain.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SyntaxWinApp01
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            TxtGender = new TextBox();
+            label4 = new Label();
+            TxtPhone = new TextBox();
+            label3 = new Label();
+            TxtAge = new TextBox();
+            label2 = new Label();
+            TxtName = new TextBox();
+            label1 = new Label();
+            BtnCheck = new Button();
+            TxtResult = new TextBox();
+            LblResult = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(TxtGender);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(TxtPhone);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(TxtAge);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(TxtName);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(271, 287);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "사람 정보 입력";
+            // 
+            // TxtGender
+            // 
+            TxtGender.Location = new Point(60, 86);
+            TxtGender.Name = "TxtGender";
+            TxtGender.Size = new Size(150, 23);
+            TxtGender.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(18, 89);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 0;
+            label4.Text = "성별 :";
+            label4.TextAlign = ContentAlignment.TopRight;
+            // 
+            // TxtPhone
+            // 
+            TxtPhone.Location = new Point(60, 115);
+            TxtPhone.Name = "TxtPhone";
+            TxtPhone.Size = new Size(150, 23);
+            TxtPhone.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 118);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 15);
+            label3.TabIndex = 0;
+            label3.Text = "핸드폰 :";
+            label3.TextAlign = ContentAlignment.TopRight;
+            // 
+            // TxtAge
+            // 
+            TxtAge.Location = new Point(60, 57);
+            TxtAge.Name = "TxtAge";
+            TxtAge.Size = new Size(150, 23);
+            TxtAge.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 0;
+            label2.Text = "나이 :";
+            label2.TextAlign = ContentAlignment.TopRight;
+            // 
+            // TxtName
+            // 
+            TxtName.Location = new Point(60, 28);
+            TxtName.Name = "TxtName";
+            TxtName.Size = new Size(150, 23);
+            TxtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "이름 :";
+            label1.TextAlign = ContentAlignment.TopRight;
+            // 
+            // BtnCheck
+            // 
+            BtnCheck.Location = new Point(472, 259);
+            BtnCheck.Name = "BtnCheck";
+            BtnCheck.Size = new Size(100, 40);
+            BtnCheck.TabIndex = 6;
+            BtnCheck.Text = "확인";
+            BtnCheck.UseVisualStyleBackColor = true;
+            BtnCheck.Click += BtnCheck_Click;
+            // 
+            // TxtResult
+            // 
+            TxtResult.BorderStyle = BorderStyle.FixedSingle;
+            TxtResult.Location = new Point(298, 47);
+            TxtResult.Multiline = true;
+            TxtResult.Name = "TxtResult";
+            TxtResult.ReadOnly = true;
+            TxtResult.Size = new Size(274, 206);
+            TxtResult.TabIndex = 5;
+            // 
+            // LblResult
+            // 
+            LblResult.AutoSize = true;
+            LblResult.Location = new Point(298, 29);
+            LblResult.Name = "LblResult";
+            LblResult.Size = new Size(31, 15);
+            LblResult.TabIndex = 0;
+            LblResult.Text = "결과";
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 311);
+            Controls.Add(BtnCheck);
+            Controls.Add(groupBox1);
+            Controls.Add(TxtResult);
+            Controls.Add(LblResult);
+            Name = "FrmMain";
+            Text = "문법학습 윈앱 01";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox TxtName;
+        private Label label1;
+        private Button BtnCheck;
+        private TextBox TxtGender;
+        private Label label4;
+        private TextBox TxtPhone;
+        private Label label3;
+        private TextBox TxtAge;
+        private Label label2;
+        private TextBox TxtResult;
+        private Label LblResult;
+    }
+}
---
+++ day03/Day03Study/SyntaxWinApp02/FrmMain.cs
@@ -0,0 +1,93 @@
+﻿using SyntaxWinApp02.Properties;
+
+namespace SyntaxWinApp02
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            //Computer myCom = new Computer();
+            //NoteBook myNotbook = new NoteBook();
+            //Server yourServer = new Server();   
+
+            //Computer yourCom = new NoteBook();      // 부모클래스에 자식 객체를 할당
+            //NoteBook notebook = new Computer(); // 자식한테 부모 X
+
+
+            switch (CboDivision.SelectedIndex)
+            {
+                case 0: // Conputer
+                    Computer myCom = new Computer();
+                    PicComputer.Image = Resources.computer_case;
+
+                    myCom.Reset();
+                    myCom.ShutDown();
+                    myCom.Boot();
+                    break;
+
+                case 1: // Notebook
+                    NoteBook myBook = new NoteBook();
+                    PicComputer.Image = Resources.laptop;
+
+                    myBook.Reset();
+                    myBook.ShutDown();  // 부모와 다르게 동작
+
+                    // Computer sCom = myBook;
+                    // 부모 클래스를 자식클래스로 형변환하면서 문제발생 소지
+                    //NoteBook myBook2 = (NoteBook)new Computer();
+                    Computer myComputer = new NoteBook();
+
+                    if (myBook is NoteBook)
+                    {
+                        Console.WriteLine("myComputer is Notebook");
+                        NoteBook myBook2 = myComputer as NoteBook;
+                        Console.WriteLine("myComputer를 NoteBook으로 변경");
+                    }
+
+                    // 지문 인식확인
+                    bool hasFinger = myBook.HasFingerScanDevice();
+                    Console.WriteLine($"최초 지문 인식여부 :: {hasFinger}");
+                    // 메서드 오버로드
+                    hasFinger = myBook.HasFingerScanDevice(true);
+                    Console.WriteLine($"최종 지문 인식여부 :: {hasFinger}");
+                    break;
+
+                case 2: // Server
+                    Server yourServ = new Server();
+                    PicComputer.Image = Resources.server;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            Server server1 = new Server();
+            server1.Name = "HP서버";
+
+            Server server2 = server1;   // 얕은 복사 : 같은 내용 참조
+            server2.Name = "DELL서버";
+
+            bool isSame = server1.Equals(server2);
+            Console.WriteLine(isSame);
+            
+            MessageBox.Show($"{server1.Name}\r\n{server2.Name}", "서버명");
+            
+            // 깊은 복사 : 완전 다른 객체로 복사
+            Server server3  = server1.DeepCopy();
+            server3.Name = "INTEL서버";
+
+            isSame = server2.Equals(server3);
+            Console.WriteLine(isSame);
+
+            MessageBox.Show($"{server1.Name}\r\n{server3.Name}", "서버명");
+
+        }
+    }
+}
