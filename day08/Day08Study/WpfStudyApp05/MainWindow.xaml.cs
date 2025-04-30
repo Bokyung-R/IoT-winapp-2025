b129487792850da6a5adf3a6e985f40637bd917f
BoKyung Ryu <dbqhrud5789@naver.com>
2025-04-30 14:26:26 +0900

    8일차 커밋

--- day08/Day08Study/WpfStudyApp05/MainWindow.xaml.cs
+++ day08/Day08Study/WpfStudyApp05/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using MahApps.Metro.Controls;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,21 +10,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfStudyApp04
+namespace WpfStudyApp05
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Hi");
         }
     }
 }
